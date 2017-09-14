ee8f0fb235440e8618116de423588465567eec1c
torrent benoit <torrentdu64@gmail.com>
2017-09-14 16:27:16 +0200

    router and scene

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -9,7 +9,7 @@ export default (state = INITIAL_STATE, action ) => {
       case PASSWORD_CHANGED:
        return { ...state, password: action.payload };
       case LOGIN_USER_SUCCESS:
-        return { ...state, user: action.payload, error: '', loading: false, email: '', password: '' };
+        return { ...state, ...INITIAL_STATE, user: action.payload };
       case LOGIN_USER:
         return { ...state, loading: true, error: ''}
       case LOGIN_USER_FAIL:
